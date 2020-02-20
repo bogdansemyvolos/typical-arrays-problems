88604b994ce207aef187d58d417930f96239c4e0
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-20 17:09:39 +0200

    init: implement typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let min = 0;
+    if (array != undefined && array.length != 0) {
+        for (let el of array) {
+            if (el < min) {
+                min = el;
+            }
+        }
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let max = 0;
+    if (array != undefined && array.length != 0) {
+        for (let el of array) {
+            if (el > max) {
+                max = el;
+            }
+        }
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let avg = 0;
+    if (array != undefined && array.length != 0) {
+        for (let el of array) {
+            avg = avg + el;
+        }
+        avg = avg / array.length
+    }
+    return avg;
 }
